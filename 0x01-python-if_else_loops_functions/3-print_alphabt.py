e84b090247c610dfc5a5355e347f90df1cafb1cd
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-03 13:17:44 +0300

    number 3 alphabet

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
-for letter in 'abcdefghijklmnopqrstuvwxyz':
-    if letter not in ('q', 'e'):
+import string
+for letter in string.ascii_lowercase:
+    if letter not in ['q', 'e']:
         print(letter, end="")
