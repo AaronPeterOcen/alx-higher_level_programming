d9f1862dce5a6e1876dc2889c1aaf1287ad7ff35
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-11 19:52:35 +0300

    task 5

--- 0x04-python-more_data_structures/5-number_keys.py
+++ 0x04-python-more_data_structures/5-number_keys.py
@@ -1,7 +1,3 @@
 #!/usr/bin/python3
 def number_keys(a_dictionary):
-    sums = 0
-    for key in a_dictionary:
-        # return sum(key)
-        sums += key
-        return sums
+    return len(a_dictionary)
