c85feac597d481e5a2090a9d4bc6bba218a6e20d
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-23 20:33:17 +0300

    task 100

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+        return True
+    except ValueError as e:
+        print(f"Exception: {e}", file=sys.stderr)
+        return False
