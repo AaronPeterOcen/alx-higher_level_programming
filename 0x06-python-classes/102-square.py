30763e3c5b865c6db6f16de044f1f76672bfacdd
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-24 20:24:12 +0300

    task 9

---
+++ 0x06-python-classes/102-square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" script that defines a square"""
+
+
+class Square:
+    """
+    Attributes:
+    size : square size
+    value : area value
+    """
+
+    def __init__(self, size=0):
+        if type(size) is not int:
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    @property
+    def size(self):
+        return self.__size
+
+    @property.setter
+    def size(self, value):
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        return self.__size ** 2
