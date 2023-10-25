5fd4b4466de6a145c88840ad695d69adb16449a6
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-25 08:42:04 +0300

    task 8

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,7 +3,7 @@ def safe_print_list(my_list=[], x=0):
     number = 0
     try:
         while number < x:
-            print(my_list[number], end='')
+            print(my_list[number], end="")
             number += 1
     except IndexError:
         pass
--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
         for i in range(x):
             element = my_list[i]
             if isinstance(element, int):
-                print("{:d}".format(element), end='')
+                print("{:d}".format(element), end="")
                 pint += 1
     except IndexError:
         raise
