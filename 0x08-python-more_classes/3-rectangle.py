3119dd4d0cac154bc7a23dc5cde97b318a3d5045
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-31 01:47:12 +0300

    task three

--- 0x08-python-more_classes/3-rectangle.py
+++ 0x08-python-more_classes/3-rectangle.py
@@ -18,8 +18,20 @@ class Rectangle:
         elif width < 0:
             raise ValueError("width must be >= 0")
         """
-        self.__width = width
-        self.__height = height
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
     @property
     def height(self):
@@ -37,14 +49,6 @@ class Rectangle:
     def width(self):
         return self.__width
 
-    @width.setter
-    def width(self, value):
-        if not isinstance(value, int):
-            raise TypeError("width must be an integer")
-        elif value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
-
     def area(self):
         """calculate the area of the rectangle object"""
         return self.__width * self.__height
@@ -53,4 +57,9 @@ class Rectangle:
         """calculate the perimeter of the rectangle"""
         if self.__width is 0 or self.__height is 0:
             return 0
-        return (self.__width * 2) + (self.__height * 2)
+        return 2 * (self.__width + self.__height)
+
+    def __str__(self):
+        if self.width == 0 or self.height == 0:
+            return ""
+        return "\n".join(["#" * self.__width] * self.__height)
