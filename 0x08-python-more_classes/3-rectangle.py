bd5cd0b37615c4fe580332e1a68bbf0d5933ee3b
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-30 12:23:18 +0300

    not fixed

---
+++ 0x08-python-more_classes/3-rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+
+""" class that defines a rectangle """
+
+
+class Rectangle:
+    """
+    Defines the Rectangel class
+    Attributes:
+    width: ...
+    height: ...
+    """
+
+    def __init__(self, width=0, height=0):
+        """
+        if type(width) is not int:
+            raise TypeError("width must be an integer")
+        elif width < 0:
+            raise ValueError("width must be >= 0")
+        """
+        self.__width = width
+        self.__height = height
+
+    @property
+    def height(self):
+        return self.__width
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__height = value
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    def area(self):
+        """calculate the area of the rectangle object"""
+        return self.__width * self.__height
+
+    def perimeter(self):
+        """calculate the perimeter of the rectangle"""
+        if self.__width is 0 or self.__height is 0:
+            return 0
+        return (self.__width * 2) + (self.__height * 2)
