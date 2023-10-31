416bc593c561494e99a7de4e6ed88fb3ba5ad23e
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-10-31 21:10:41 +0300

    task

---
+++ 0x09-python-everything_is_object/py.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+l1 = [1, 2, 3]
+l2 = l1
+l1 = l1 + [4]
+print(l2)
+
+def increment(n):
+    n += 1
+
+a = 1
+increment(a)
+print(a)
+
+def increment(n):
+    n.append(4)
+
+l = [1, 2, 3]
+increment(l)
+print(l)
