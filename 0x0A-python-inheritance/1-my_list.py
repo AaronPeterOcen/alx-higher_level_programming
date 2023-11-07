2fb16b8c16b0f9ed167b29aa3631b59e82036a19
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-11-07 08:08:45 +0300

    task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ .. """
 
+
 class MyList(list):
-    """ .."""
-    
+    """.."""
+
     def print_sorted(self):
-        """ .."""
+        """.."""
         print(sorted(self))
