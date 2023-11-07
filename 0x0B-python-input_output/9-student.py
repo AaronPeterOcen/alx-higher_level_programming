abb6663f5dfbefa8ba92e4572e72de9ab55678d4
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-11-07 12:37:22 +0300

    task 9

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -7,6 +7,7 @@ class Student:
     """new class student"""
 
     def __init__(self, first_name, last_name, age):
+        """initialise student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
