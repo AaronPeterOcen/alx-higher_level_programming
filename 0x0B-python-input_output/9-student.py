7f4b39d269d2cad48779de1be5e3f5a174a6311c
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-11-07 12:36:50 +0300

    task 9

---
+++ 0x0B-python-input_output/9-student.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" new class student"""
+import json
+
+
+class Student:
+    """new class student"""
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, my_list):
+        """new method to_json"""
+        return json.dumps(my_list)
