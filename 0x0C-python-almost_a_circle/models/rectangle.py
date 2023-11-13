fa3c673b6ac14c28b4630320d82e1a42ae6189ff
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-11-13 08:52:02 +0300

    task 5

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -97,3 +97,16 @@ class Rectangle(Base):
         return area of rectangle
         """
         return self.__width * self.__height
+
+    def display(self):
+        """
+        prints in stdout the Rectangle instance with the character #
+        """
+        for i in range(self.__y):
+            print()
+        for i in range(self.__height):
+            for j in range(self.__x):
+                print(" ", end="")
+            for j in range(self.__width):
+                print("#", end="")
+            print()
