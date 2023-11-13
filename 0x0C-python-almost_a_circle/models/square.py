b6ff8e2b078b6651737ef9d3db93f6ab73d6cc5a
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2023-11-13 08:59:46 +0300

    task 10 square

---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+"""
+new class square
+"""
+from models.rectangle import Rectangle
+
+
+class Square(Rectangle):
+    """
+    new class square
+    """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """
+        init
+        """
+        super().__init__(size, size, x, y, id)
+        self.size = size
+        self.x = x
+        self.y = y
+        self.id = id
+
+    def __str__(self):
+        """
+        str
+        """
+        return "[Square] ({}) {}/{} - {}".format(self.id, self.x, self.y, self.size)
+
+    @property
+    def size(self):
+        """
+        size
+        """
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        """
+        size setter
+        """
+        self.width = value
+        self.height = value
+
+    # def update(self, *args, **kwargs):
+    #     """
+    #     update
+    #     """
+    #     if args:
+    #         for i, arg in enumerate(args):
+    #             if i == 0:
+    #                 self.id = arg
