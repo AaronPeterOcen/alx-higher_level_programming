b31f32cb299654a4553e1acc119a0e79cb1a23bc
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-08 19:28:16 +0300

    task 0 print text

---
+++ 0x12-javascript-warm_up/basics/index.js
@@ -0,0 +1,13 @@
+// console.log("lorem ipsum");
+
+// let name = "tyeen";
+
+// strings, integers
+
+const interestRate = 0.3;
+
+// interestRate = 2;
+
+console.log(interestRate);
+
+// look at primitive and reference types
