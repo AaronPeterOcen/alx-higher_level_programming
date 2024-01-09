89389bfde362488557bd597aabb308a13f08d8b6
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-09 20:21:29 +0300

    task 1 fix

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -7,7 +7,7 @@
 // Initialize the instance attribute height with the value of h
 
 module.exports = class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     this.width = w;
     this.height = h;
   }
