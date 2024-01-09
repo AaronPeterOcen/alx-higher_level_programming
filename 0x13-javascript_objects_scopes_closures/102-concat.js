fab0677f72d23250b70047565c9d0452d1606876
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-09 21:51:51 +0300

    semistandard fix

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -5,7 +5,7 @@
 // The second argument is the file path of the second source file
 // The third argument is the file path of the destination
 
-const fs = require("fs");
-const file1 = fs.readFileSync(process.argv[2], "utf8");
-const file2 = fs.readFileSync(process.argv[3], "utf8");
+const fs = require('fs');
+const file1 = fs.readFileSync(process.argv[2], 'utf8');
+const file2 = fs.readFileSync(process.argv[3], 'utf8');
 fs.writeFileSync(process.argv[4], file1 + file2);
