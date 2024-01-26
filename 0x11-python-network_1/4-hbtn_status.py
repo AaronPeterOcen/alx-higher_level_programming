023559250cd823c2e614406e6079815b0bda95c8
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-01-26 20:09:10 +0300

    task 4 "whats my status"

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+"""
+script that fetches https://alx-intranet.hbtn.io/status
+"""
+import requests
+
+if __name__ == "__main__":
+    rq = requests.get("https://alx-intranet.hbtn.io/status")
+    print("Body response:")
+    print("\t- type: {}".format(type(rq.text)))
+    print("\t -content: {}".format(rq.text))
