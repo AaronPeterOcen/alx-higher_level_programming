a7feb47c9bb831ae18b4f8bcbe15d6b088b3a681
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-02-28 23:12:17 +0300

    update on tasks 2, 3, 4

--- 0x15-javascript-web_jquery/4-script.js
+++ 0x15-javascript-web_jquery/4-script.js
@@ -1,6 +1,8 @@
 // Toggles the class of HTML tag 'HEADER' when user clicks the
 // div#toggle_header tag
 
-$("div#toggle_header").click(function () {
-  $("header").ToggleClass("red");
+$(document).ready(function () {
+    $('#toggle_header').click(function () {
+        $('header').toggleClass('red green');
+    });
 });
