fce6e5d999dbd7829af5b08009adc82e4115979c
AaronPeterOcen <ocenaaronpeter2001@gmail.com>
2024-02-28 23:26:30 +0300

    update on the tasks

--- 0x15-javascript-web_jquery/8-script.js
+++ 0x15-javascript-web_jquery/8-script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    $.getJson("", function(data) {
+    $.getJson("https://swapi-api.alx-tools.com/api/films/?format=json", function(data) {
         $.each(data.results, function(index, movie) {
             $("#list_movies").append("<li>" + movie.title + "</li>");
         });
